237a31e311b902e86b9f0ae1925bf31951e807b5
“RotimisGitHub” <rotimijatto03@gmail.com>
2023-11-16 20:16:29 +0000

    another push

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['touchedbynoa-57ca3931f654.herokuapp.com']
+ALLOWED_HOSTS = ['touchedbynoa-57ca3931f654.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
